2b27c3affdff174f47cddb9eb772711048c6b652
Solowd1986 <solowd@yandex.ru>
2019-11-22 23:10:08 +0300

    Krasnov

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,64 @@
 
-"use strict";
+
+const gulp = require('gulp');
+
+let browserSync = require('browser-sync').create();
+let nunjucks = require('gulp-nunjucks');
+let rename = require("gulp-rename");
+let requireDir = require("require-dir");
+let renderNjk = require("gulp-nunjucks-render");
+
+const path = {
+    build: {
+        base: './',
+        html_index: 'dist',
+        html_pages: 'dist/pages',
+        css: 'dist/assets/css',
+        js: 'dist/assets/js',
+        img: 'dist/assets/img'
+    },
+
+    src: {
+        css: 'src/assets/scss/**/*.css',
+        scss: 'src/assets/scss/**/*.scss',
+        js: 'src/assets/js/**/*.js',
+        tpl: 'src/tpl/**/*.njk',
+        tpl_hide: '!src/tpl/**/_*.njk',
+        img: 'src/assets/img/**/*.{png, jpeg, jpg, svg, gif, webp}'
+    },
+
+    watch: {
+        tpl: 'src/tpl/**/*.njk',
+        scss: 'src/assets/scss/**/*.scss',
+        css: 'src/assets/scss/**/*.css',
+        html: './*.html'
+    },
+
+    clean: 'dist'
+};
 
 
 
+gulp.task('build', function() {
+    return gulp.src([path.src.tpl, path.src.tpl_hide])
+        .pipe(nunjucks.compile())
+        .pipe(rename({
+            extname: ".html"
+        }))
+
+        .pipe(gulp.dest(path.build.base))
+});
+
+
+gulp.task('default', ['build'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch([path.watch.tpl, path.watch.scss], ['build']);
+    gulp.watch([path.watch.html, path.watch.css]).on('change', browserSync.reload);
+});
 
 
 
